7714f18186e998eb52f7dab79961061ad1701068
Michael Bennett <mvbennett9@gmail.com>
2023-01-22 14:11:07 -0500

    made a slightly different class that initializes with string instead

--- class_recursion.rb
+++ class_recursion.rb
@@ -1,20 +1,43 @@
+# class Permutations
+#   def initialize
+#     @sum = []
+#   end
+#
+#   def combos(build, depth, string)
+#     return @sum << build if depth == string.length
+#
+#     combos(build, depth + 1, string)
+#     combos(build += string[depth], depth + 1, string)
+#   end
+#
+#   def get_permutations(string)
+#     combos('', 0, string)
+#     return @sum
+#   end
+# end
+#
+# test_case = Permutations.new
+# p test_case.get_permutations('abc')
+
+
 class Permutations
-  def initialize
+  def initialize(string)
+    @string = string
     @sum = []
   end
 
-  def combos(build, depth, string)
-    return @sum << build if depth == string.length
+  def combos(build, depth)
+    return @sum << build if depth == @string.length
 
-    combos(build, depth + 1, string)
-    combos(build += string[depth], depth + 1, string)
+    combos(build, depth + 1)
+    combos(build += @string[depth], depth + 1)
   end
 
-  def get_permutations(string)
-    combos('', 0, string)
+  def get_permutations
+    combos('', 0)
     return @sum
   end
 end
 
-test_case = Permutations.new
-p test_case.get_permutations('abc')
+test_case = Permutations.new('abc')
+p test_case.get_permutations
