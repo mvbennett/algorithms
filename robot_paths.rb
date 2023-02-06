ccbc3c545b39e2f2d67ebe6432a1aa9d810777aa
Michael Bennett <mvbennett9@gmail.com>
2023-02-06 12:50:31 -0500

    made a robot paths method using toggle on matrix

---
+++ robot_paths.rb
@@ -0,0 +1,20 @@
+def robotPaths(matrix)
+  paths = 0
+
+  find_paths = lambda do |row, col|
+      return if row < 0 || col < 0 || row >= matrix.length || col >= matrix.first.length
+      return if matrix[row][col] == 1
+      return paths += 1 if row == matrix.length - 1 && col == matrix.first.length - 1
+
+      matrix[row][col] = 1
+
+      find_paths.call(row + 1, col)
+      find_paths.call(row, col + 1)
+      find_paths.call(row - 1, col)
+      find_paths.call(row, col - 1)
+
+      matrix[row][col] = 0
+  end
+  find_paths.call(0, 0)
+  paths
+end
