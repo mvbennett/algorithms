4d94722621ecf703258840bc11e59561fb74a7f1
Michael Bennett <mvbennett9@gmail.com>
2023-02-07 02:43:05 -0500

    made better tests for binary search tree

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,4 +1,5 @@
-=begin
+# frozen_string_literal: true
+
 # Given a binary tree root node, check if the tree is a valid binary search tree.
 #
 # Input: Node in a Binary Tree
@@ -40,15 +41,14 @@
 #
 #
 
-Diagram 2:
-helper(node, min, max)
+# Diagram 2:
+# helper(node, min, max)
 #     5 c
 #    / \
 #   2 c   7c
 #       /  \
 #       4 c  9
-
-
+#
 # Pseudocode:
 # make array values
 # make function helper(node)
@@ -56,16 +56,16 @@ helper(node, min, max)
 #     return
 #
 #   if helper(node.left) > Node:
-      return false
+#      return false
 #   if helper(node.right) < node:
-      return false
+#      return false
 #   return node.value
 #
 
-
-=end
+# Node class
 class Node
   attr_accessor :value, :left, :right
+
   def initialize(value = nil)
     @value = value
     @left = nil
@@ -93,9 +93,9 @@ root.right = Node.new(7)
 root.right.right = Node.new(9)
 root.right.left = Node.new(4)
 
-puts "expect: false"
+# puts "expect: false"
 
-p check_tree(root)
+# p check_tree(root)
 
 root_two = Node.new(5)
 root_two.left = Node.new(2)
@@ -103,13 +103,21 @@ root_two.right = Node.new(7)
 root_two.right.right = Node.new(9)
 root_two.right.left = Node.new(6)
 
-puts "expect: true"
-p check_tree(root_two)
+# puts "expect: true"
+# p check_tree(root_two)
 
-# class BinaryTree
-#   def initialize
-#     @root = nil
-#     @size = 0
-#   end
+describe check_tree(root) do
+  context 'when given a tree that is not a valid binary search tree' do
+    it 'should return false' do
+      expect(check_tree(root)).to be_falsy
+    end
+  end
+end
 
-# end
+describe check_tree(root_two) do
+  context 'when given a tree that is a valid binary search tree' do
+    it 'should return true' do
+      expect(check_tree(root_two)).to be_truthy
+    end
+  end
+end
