49679f7ac1eb986911f3714f15c4bae6961949b0
Michael Bennett <mvbennett9@gmail.com>
2023-03-04 22:00:32 -0500

    made a simple add method for heap

--- heap_class.rb
+++ heap_class.rb
@@ -1,43 +1,57 @@
 class Heap
-  def initalize
+  attr_reader :array
+
+  def initialize
     @array = []
   end
 
-  def child_index(arr, parent_index, bounds)
+  def child_index(parent_index, bounds)
     child_i_one = 2 * parent_index + 1
     child_i_two = 2 * parent_index + 2
-    return child_i_one if child_i_one >= bounds || child_i_two >= bounds || arr[child_i_one] > arr[child_i_two]
+    return child_i_one if child_i_one >= bounds || child_i_two >= bounds || @array[child_i_one] > @array[child_i_two]
 
     child_i_two
   end
 
-  def swap(arr, index1, index2)
-    arr[index1], arr[index2] = arr[index2], arr[index1]
+  def swap(index1, index2)
+    @array[index1], @array[index2] = @array[index2], @array[index1]
   end
 
-  def bubble_down(arr, parent_index, bounds)
-    child_index = child_index(arr, parent_index, bounds)
+  def bubble_down(parent_index, bounds)
+    child_index = child_index(parent_index, bounds)
 
-    while child_index < bounds && arr[parent_index] < arr[child_index]
-      swap(arr, parent_index, child_index)
+    while child_index < bounds && @array[parent_index] < @array[child_index]
+      swap(parent_index, child_index)
       parent_index = child_index
-      child_index = child_index(arr, parent_index, bounds)
+      child_index = child_index(parent_index, bounds)
     end
   end
 
-  def heapsort(arr)
-    i = arr.length - 1
+  def heapsort
+    i = @array.length - 1
     while i >= 0
-      bubble_down(arr, i, arr.length)
+      bubble_down(i, @array.length)
       i -= 1
     end
 
-    wall = arr.length - 1
+    wall = @array.length - 1
     while wall >= 0
-      swap(arr, 0, wall)
-      bubble_down(arr, 0, wall)
+      swap(0, wall)
+      bubble_down(0, wall)
       wall -= 1
     end
-    arr
+    @array
+  end
+
+  def add(value)
+    @array << value
+    heapsort
   end
 end
+
+heap = Heap.new
+nine_nums = [5, 2, 3, 9, 1, 4, 6, 8, 7]
+nine_nums.each do |num|
+  heap.add(num)
+end
+p heap.array
